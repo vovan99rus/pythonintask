78e0328e6c8f5136f88098d82e67522501fee685
Akexandre <boss.kinder1997@.mail.ru>
2016-03-28 15:15:12 +0300

    Ермашов.А.8 Задача 4 Вариант 5. Решено

---
+++ INBa/2015/Ermashov_A_V/task_4_5.py
@@ -0,0 +1,16 @@
+﻿# Ermashov.A.V.
+# 28.03.2016
+
+name = "Фредерик Аустерлиц"
+birthplace = "Омахе, штат Небраска"
+birthyear = int (1899)
+deathyear = int (1987)
+age = int (deathyear - birthyear)
+interest = "Актёр,певец"
+print(name+" наиболее известен как Фред Астер - американский актёр,\nтанцор,хореограф и певец,звезда Голливуда.")
+print("Место рождения: "+birthplace)
+print("Год рождения: "+str(birthyear))
+print("Год смерти: "+str(deathyear))
+print("Возраст смерти: "+str(age))
+print("Область интересов: "+interest)
+input("\nДля выхода нажмите Enter") 
