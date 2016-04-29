09d045a5bec8c450e33d7dcbe202a4c1d1ae3d36
Denis007138 <grovestreet@inbox.ru>
2016-04-29 12:45:00 +0300

    Андрос Д.А.Задача 6.Вариант 2.

---
+++ PMIa/2015/ANDROS_D_A/task_6_2.py
@@ -0,0 +1,12 @@
+﻿#Задача 6.Вариант 2.
+#Создайте игру, в которой компьютер загадывает название одного из двенадцати созвездий, входящих в зодиакальный круг, а игрок должен его угадать.
+#Andros D.A.
+#28.04.2016
+import random
+sozvezdia=["Oven","Telec","Bliznetsy","Rak","Lev","Deva","Vesy","Skorpion","Strelec","Kozerog","Vodoley","Riby"]
+s=random.choice(sozvezdia)
+a=input("Введите 1 из созвездий")
+if a==s:
+   print("exactly")
+else:
+   print("you wrong")
