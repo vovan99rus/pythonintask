3c9c8bdbd592b97b0035be75ae029aaf1d616fd0
SobolMax <priest321@mail.ru>
2016-05-23 17:29:23 +0300

    Create task_4_24.py

---
+++ IVTp/2014/SOBOLEV_M_V/task_4_24.py
@@ -0,0 +1,12 @@
+print("Ефим Алексеевич -  русский промышленный инженер")
+rod = "Россия, Нижний Тагил";
+print("Место рождения: "+live)
+godz = 1774
+smert =1842
+print("Годы жизни: "+godz+'-'+smert)
+age = smert-godz
+print("Возраст: "+age)
+interesy = "Инженерия"
+print("Область интересов: "+interes)
+letsmomentsmert = 2016 - smert
+print('Лет с момента смерти: '+letsmomentsmert)
