3caa7dd872f9c4c6faf170f0500f8e6516868f61
lexusdonkor <lexusdonkor@gmail.com>
2016-05-27 19:52:48 +0400

    Донкор Алекс.Хиксон. Задача 8,9,12. Вариант 10. Задачи Решены

---
+++ PMIa/2015/Donkor_A_H/task_12_10.py
@@ -0,0 +1,156 @@
+﻿#Задача 12. Вариант 10.
+#Разработайте игру "Крестики-нолики". (см. М.Доусон Программируем на Python гл. 6).
+
+#Donkor.A.H.
+#27.05.2016
+
+import random
+
+print("\n\nЗдравствуйте приветствуем вас в самой развлекающей, азартной и логической игре - Крестики-Нолики.")
+print("Игра считается логической, в ней нужно продумывать ходы и много мыслить.")
+
+print("Игра будет законченной только тогда, когда мы определим победивших или проигравших или же вы можете сыграть в ничью.")
+
+print("\nДавайте начинать!!!")
+
+X = "X"
+O = "O"
+EMPTY = " "
+TIE = "ничья"
+NUM_SQUARES = 9
+def main():
+	return 0
+def display_instruct():
+	print ("""
+	0 | 1 | 2
+	---------
+	3 | 4 | 5
+	---------
+	6 | 7 | 8
+	""")
+def ask_yes_no(question):
+	response = None
+	while response not in ("y", "n"):
+		response = input (question).lower()
+	return response
+	
+def ask_number(question, low, high):
+	response = None
+	while response not in range(low, high):
+		response = int(input(question))
+	return response
+def pieces():
+	go_first = ask_yes_no("Кто будет ходить первым?\n")
+	if go_first == "y":
+		print ("Хорошо, Ваш ход!")
+		human = X
+		computer = O
+	else:
+		print ("Я,Начинаю")
+		human = O
+		computer = X
+	return computer, human
+	
+def new_board():
+	board = []
+	for square in range(NUM_SQUARES):
+		board.append(EMPTY)
+	return board
+	
+def display_board(board):
+	print ("\n\t", board[0], " | ", board[1], " | ", board[2])
+	print ("\t", "-----------")
+	print ("\n\t", board[3], " | ", board[4], " | ", board[5])
+	print ("\t", "-----------")
+	print ("\n\t", board[6], " | ", board[7], " | ", board[8])
+	print ("\t", "-----------\n")
+	
+def legal_moves(board):
+	moves =[]
+	for square in range(NUM_SQUARES):
+		if board[square] == EMPTY:
+			moves.append(square)
+	return moves
+	
+def winner(board):
+	WAYS_TO_WIN = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+	for row in WAYS_TO_WIN:
+		if board[row[0]] == board[row[1]] == board[row[2]] != EMPTY:
+			winner = board[row[0]]
+			return winner
+		if EMPTY not in board:
+			return TIE
+	return None
+	
+def human_move(board, human):
+	legal = legal_moves(board)
+	move = None
+	while move not in legal:
+		move = ask_number("Итак, ваш ход ", 0, NUM_SQUARES)
+		if move not in legal:
+			print ("\nК сожалению, поле уже занято.\n")
+	print ("OK.")
+	return move
+	
+def computer_move(board, computer, human):
+	#Итак, создаем копию доски
+	board = board[:]
+	# Тут приоритет полей
+	BEST_MOVES = (4, 0, 2, 6, 8, 1, 3, 5, 7)
+	print ("Ход на поле номер", end=" ")
+	for move in legal_moves(board):
+		board[move] = computer
+		#Следовательно, если ход ведет к победе, то выбираем его
+		if winner(board) == computer:
+			print (move)
+			return move
+		board[move] = EMPTY
+	for move in legal_moves(board):
+		board[move] = human
+		if winner(board) == human:
+			print (move)
+			return move
+		board[move] = EMPTY
+	for move in BEST_MOVES:
+		if move in legal_moves(board):
+			print (move)
+			return move
+def next_turn(turn):
+	if turn == X:
+		return O
+	else:
+		return X
+		
+def congrat_winner(the_winner, computer, human):
+	if the_winner != TIE:
+		print (the_winner, "побеждают\n")
+	else:
+		print ("Ничья")
+	if the_winner == computer:
+		print ("\nПобеда самого развитого в мире компьютера\n")
+	elif the_winner == human:
+		print ("\nПобеда наиумнейшего человека во вселенной\n")
+	elif the_winner == TIE:
+		print ("\n Поздравляю у вас Ничья")
+def game():
+	display_instruct()
+	computer, human = pieces()
+	turn = X
+	board = new_board()
+	display_board(board)
+	while not winner(board):
+		if turn == human:
+			move = human_move(board, human)
+			board[move] = human
+		else:
+			move = computer_move(board, computer, human)
+			board[move] = computer
+		display_board(board)
+		turn = next_turn(turn)
+	the_winner = winner(board)
+	congrat_winner(the_winner, computer, human)
+	
+if __name__ == '__main__':
+	main()
+	game()
+	input ("\nНажмите ENTER, чтобы завершить")
