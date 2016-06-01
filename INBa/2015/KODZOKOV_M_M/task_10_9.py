62e5fe5206b3d36bc1052701065eeac2498b67c5
Murat <Kukaracha090298@gmail.com>
2016-06-01 19:24:28 +0300

    Кодзоков М.М. Задача 10. Вариант 9.

---
+++ INBa/2015/KODZOKOV_M_M/task_10_9.py
@@ -0,0 +1,48 @@
+#Задача 10. Вариант 9
+#Напишите программу "Генератор персонажей" для игры. Пользователю должно быть предоставлено 30 пунктов, которые можно распределить между четырьмя характеристиками: Сила, Здоровье, Мудрость и Ловкость. Надо сделать так, чтобы пользователь мог не только брать эти пункты из общего "пула", но и возвращать их туда из характеристик, которым он решил присвоить другие значения.
+
+#Kodzokov M.M.
+#21.05.2016
+print("Генератор персонажа")
+points = 30
+character = {"Сила" : 8, "Ловкость" : 8, "Здоровье" : 8, "Мудрость" : 8}
+for i in character:
+	print(character[i])
+values = character.keys()
+atr = input("Введите атрибут, который вы хотите изменить: ")
+num = int(input("Введите количество очков (\"-\" для вычитания очков): "))
+print(num)
+points -= num
+for value in character:
+	if attr == value:
+		print(atr)
+
+class Character:
+	def __init__(self, points):
+		self.atrib = ("Сила", "Ловкость", "Здоровье", "Мудрость")
+		self.values = [8, 8, 8, 8]
+		self.ppool = points
+	def update(self, chrt, num):
+		print()
+		for i in range (len(self.atrib)):
+			if chrt.capitalize() == self.atrib[i]:
+				self.ppool -= num
+				if self.ppool < 0:
+					num += self.ppool
+					self.ppool = 0
+				self.values[i] += num
+			print(self.atrib[i], end = ' : ')
+			print(self.values[i])
+		print("\nОчки:", self.ppool)
+
+pers = Character(30)
+chrt, num = "some", 0
+while chrt != "":
+	pers.update(chrt, num)
+	chrt = input("\nХарактеристика: ").strip()
+	temp = input("Очки (\"-\" для уменьшения, Enter для выхода): ").strip()
+	if len(temp):
+		if temp.isdigit() or temp[0] == '-':
+			num = int(temp)
+print("Создание персонажа завершено...")
+input("\nНажмите Enter для выхода")
