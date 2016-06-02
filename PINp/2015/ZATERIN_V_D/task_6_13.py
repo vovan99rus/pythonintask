074c975c3aa7c009dcdf39145b6d83ea4222f898
Александр <canumba@mail.ru>
2016-06-02 18:41:47 +0400

    Затерин Владимир сдал работы

---
+++ PINp/2015/ZATERIN_V_D/task_6_13.py
@@ -0,0 +1,16 @@
+﻿#Создайте игру, в которой компьютер загадывает название одного из двух
+#спутников Марса, а игрок должен его угадать.
+
+import random
+sputnik = random.randint(1,2)
+if sputnik == 1: sputnik="фобос"
+elif sputnik ==2: sputnik="деймес"
+
+print("Угадай спутник Марса: ")
+otvet = input("\n ВВеди слово: ")
+while(otvet != cvet):
+    print("\nНеправильно. Попробуй еще раз")
+    otvet = input("\nВведи слово: ")
+ 
+print("Правильно! Это", sputnik,"спутник!")
+input("Нажмите ENTER для продолжения")
---
+++ PINp/2015/ZATERIN_V_D/task_7_13.py
@@ -0,0 +1,37 @@
+﻿import random
+
+print("Компьютер загадал марку одной из машин.\n")
+
+car = ('chevralet','honda','mitsubishi','dodge','bmw','mercedes')
+car = random.randint(0,5)
+x = 0
+i = 0
+score = 0
+
+#print (car[0]\n,car[1]\n,car[2]\n,car[3]\n,car[4]\n,car[5])
+
+while(x != 6):
+	print(auto[x])
+	x += 1
+
+answer = input("\nВведите название автомобиля: ")
+
+while(answer != cars[car]):
+    print("Неверно, попробуйте ещё раз.")
+    answer = input("\n Введите название автомобиля: ")
+    i += 1
+
+if i == 0:
+	score = 100
+	
+elif 0<i<6:
+	score = 100 - i*2
+	
+else:
+	score = 0
+
+print("Верно, Вы победили!")
+print("Число попыток: "+str(i))
+print("Вы заработали "+str(score)+" баллов")
+
+input("\nДля выхода нажмите Enter.")
